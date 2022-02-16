7346fd55b83f1e03a9693e8adf5e6ed267927cc5
fuckmylife <98694702+fuxkmylife@users.noreply.github.com>
2022-02-16 21:48:18 +0800

    Add files via upload

---
+++ precache-manifest.d66e2700f2ca6f1a42e3e6242152ebe9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047ba33aed2d3b71881ccb490be1e8ed",
+    "url": "/starcatchers_catchgame/index.html"
+  },
+  {
+    "revision": "40b2799a9a76d3f1a33f",
+    "url": "/starcatchers_catchgame/static/css/2.f1bd3b54.chunk.css"
+  },
+  {
+    "revision": "3cf98802ff7614b8d5b5",
+    "url": "/starcatchers_catchgame/static/css/main.b18b17fe.chunk.css"
+  },
+  {
+    "revision": "40b2799a9a76d3f1a33f",
+    "url": "/starcatchers_catchgame/static/js/2.7372b25b.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/starcatchers_catchgame/static/js/2.7372b25b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cf98802ff7614b8d5b5",
+    "url": "/starcatchers_catchgame/static/js/main.027d71b7.chunk.js"
+  },
+  {
+    "revision": "4e46aa7f6bb0b6d941d6",
+    "url": "/starcatchers_catchgame/static/js/runtime-main.b91f0c8e.js"
+  },
+  {
+    "revision": "45001e8e248f10df963544197ac34f15",
+    "url": "/starcatchers_catchgame/static/media/SC_game_wabisabi-02-min.45001e8e.png"
+  },
+  {
+    "revision": "4d12e096f096b175ac2dcc8d28edbbe4",
+    "url": "/starcatchers_catchgame/static/media/SC_game_wabisabi-06-min.4d12e096.png"
+  }
+]);
